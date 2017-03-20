23ca7556bd2814549d309ba1e249a26cb1fe4fb1
Douglas Everton de Oliveira <douglas.oliveirabh@gmail.com>
2017-03-19 23:15:56 -0300

    Correios and ViaCep working

--- test/DotCep.Tests/Tests/CepTest.cs
+++ test/DotCep.Tests/Tests/CepTest.cs
@@ -15,24 +15,16 @@ namespace DotCep.Tests
              this.cepService = new CepService();
          }
 
-/*
+
         [Fact]
         public void Should_Find_Invalid_Cep()
         {                
             Assert.ThrowsAsync<AddressNotFoundException>(async () => await cepService.GetAddressByCep("89198198198198"));
-        }*/
+        }
 
         [Fact]
         public void Shoud_Find_ValidCep(){
             var t = Task.Run(() => cepService.GetAddressByCep("30150221"));
-
-            if(t.Result != null)
-                System.Console.Write(t.Result.ToString());
-            else
-                System.Console.Write("deu erro");
-
-
-
             Assert.True(t.Result as Address != null);             
         }
 
